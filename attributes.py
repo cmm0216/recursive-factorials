e2546431c55b8247ab9c0cc050e7172eda0b911d
Collin Modelski <cmm0216@auburn.edu>
2022-05-04 18:05:35 -0500

    Fixed to take in FASTA file from CL as input

--- attributes.py
+++ attributes.py
@@ -3,16 +3,29 @@
 #test file for the recursive-factorials assignment
 import sys
 import re
+import argparse
 from Bio import SeqIO
 
-headers = []
-def ID
-	with open("Aubie.fasta", "r") as f:
-    	for record in SeqIO.parse(f, "fasta"):
+filename = sys.argv[-1]
+
+def ID(filename):
+	"""
+	Using SeqIO, the parse function takes in Fasta file, and can parse the file into different components.
+	Seq variable created, called "record"
+
+	Input
+	______
+
+	"""
+	headers = []
+	with open(filename,'r') as f:
+    	 for record in SeqIO.parse(f, "fasta"):
         	headers.append(record.description)
 	print(headers)
 	n = len(headers)
 	print("n (the number of sampled individuals = " + str(n)) 
+
+
 
 
 
@@ -27,6 +40,6 @@ def ID
 #			str = seq.findall
 #			print(str)
 #
-#	if __name__ == "__main__":
-#		ID()
-#		date()
+
+if __name__ == "__main__":
+	ID(filename)
