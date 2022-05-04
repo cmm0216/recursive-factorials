feb59233793b0a99d33b15baa6143cc08a8f3def
Collin Modelski <cmm0216@auburn.edu>
2022-05-04 17:23:56 -0500

    n variable generated, names parsed from sequences

---
+++ attributes.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+#test file for the recursive-factorials assignment
+import sys
+import re
+from Bio import SeqIO
+
+headers = []
+def ID
+	with open("Aubie.fasta", "r") as f:
+    	for record in SeqIO.parse(f, "fasta"):
+        	headers.append(record.description)
+	print(headers)
+	n = len(headers)
+	print("n (the number of sampled individuals = " + str(n)) 
+
+
+
+#class attributes():
+#	def ID():
+#		for line in SeqIO.parse("Aubie.fasta", "fasta"):
+#			start = line(r'^>')
+#			return wc -l
+#
+#	def date():
+#		for seq in SeqIO.parse("Aubie.fasta", "fasta"):
+#			str = seq.findall
+#			print(str)
+#
+#	if __name__ == "__main__":
+#		ID()
+#		date()
